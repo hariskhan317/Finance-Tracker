31cdb86c06b3243d148517e6eef47ee1848aeaea
Haris <harisgujjar317@gmail.com>
2024-05-25 16:32:31 -0400

    deploying on vercel

--- frontend/src/context/FinanceContext.jsx
+++ frontend/src/context/FinanceContext.jsx
@@ -1,11 +1,11 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { addBudgetApi, getBudgetApi, addExpenseApi, getExpenseApi, deleteExpenseApi, deleteBudgetApi } from '../helper/apiCommunicator';
-// import { useAuth } from "./AuthContext";
+import { useAuth } from "./AuthContext";
  
 const FinanceContext = createContext(null);
 
 export const FinanceProvider = ({ children }) => {
-    //const auth = useAuth();
+    const auth = useAuth();
     const [budgets, setBudgets] = useState([]);
     const [expenses, setExpenses] = useState([]);
     const [refresh, setRefresh] = useState(false);
@@ -51,6 +51,11 @@ export const FinanceProvider = ({ children }) => {
 
         getBudgetList();
         getExpenseList();
+
+        if (auth.islogin) {
+            getBudgetList();
+            getExpenseList();
+        }
 
     }, [refresh])
 
