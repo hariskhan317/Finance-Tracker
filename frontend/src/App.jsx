96d8caf1d33e293579c85fad2233a82c14c15171
Haris <harisgujjar317@gmail.com>
2024-05-27 18:20:40 -0400

    deploying on vercel 048

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import Auth from './pages/auth.jsx';
 import FinanceTracker from './pages/financetracker.jsx';
-// import PageNotFound from './components/shared/PageNotFound.jsx'
+//import PageNotFound from './components/shared/PageNotFound.jsx'
 import Header from './components/shared/Header.jsx' 
 import { useAuth } from './context/AuthContext.jsx';
-// import PrivateRoutes from './utils/PrivateRoutes.jsx';
+import PrivateRoutes from './utils/PrivateRoutes.jsx';
 
 function App() {   
   const auth = useAuth();
