3c7325af95381f7376eea682e8c5877dce4b3901
Haris <harisgujjar317@gmail.com>
2024-05-27 13:40:31 -0400

    deploying on vercel 027

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -22,7 +22,7 @@ export const AuthProvider = ({ children }) => {
         }
     }, []);
   
-    const signup = useCallback(async (name, email, password) => {
+    const signup = async (name, email, password) => {
         try {
             const data = await userApiSignup(name, email, password);
             if (data.status === 200) {
@@ -34,9 +34,9 @@ export const AuthProvider = ({ children }) => {
         } catch (error) {
             console.error("Error during signup:", error);
         }
-    }, [handleAuthStatus]);
+    }
 
-    const login = useCallback(async (email, password) => {
+    const login = async (email, password) => {
         try {
             const data = await userApiLogin(email, password);
             if (data.status === 200) {
@@ -48,7 +48,7 @@ export const AuthProvider = ({ children }) => {
         } catch (error) {
             console.error("Error during login:", error);
         }
-    }, [handleAuthStatus]);
+    }
 
     const logout = useCallback(async () => {
         try {
