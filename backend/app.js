eb94d39e8ecd4e5b39b8c62a85b3fc35d7e1bfd3
Haris <harisgujjar317@gmail.com>
2024-05-28 15:18:40 -0400

    deploying on vercel -002

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,19 @@
 import express from 'express';
+import appRouter from './routes/index.js';
 import { config } from 'dotenv';
 import cookieParser from 'cookie-parser';
-import appRouter from './routes/index.js';
-import {mongooseConnection} from './database/index.js'
-import cors from 'cors' 
+import cors from 'cors'
+
 const app = express();
 
-config();
 
+config();
 app.use(cors({
     origin: ["https://finance-tracker-nine-murex.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true
 }));
-
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.json());
 app.use('/api/v1', appRouter);
-
-
-mongooseConnection()
-    .then(() => console.log('Connected successfully'))
-    .catch((error) => console.log("Can't connect", error))
-
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-    try {
-        console.log(`listening to port ${PORT}`);
-    }catch (error) {
-        console.log(error);
-    }
-})
+export default app;
