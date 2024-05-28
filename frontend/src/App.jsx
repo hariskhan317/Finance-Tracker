2c0d7efff103536c67e4545d315aed5bd55ea373
Haris Khan <88081332+hariskhan317@users.noreply.github.com>
2024-05-27 21:40:57 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Auth from './pages/auth.jsx';
 import FinanceTracker from './pages/financetracker.jsx';
-// import PageNotFound from './components/shared/PageNotFound.jsx'
+import PageNotFound from './components/shared/PageNotFound.jsx'
 import Header from './components/shared/Header.jsx' 
 import { useAuth } from './context/AuthContext.jsx';
 // import PrivateRoutes from './utils/PrivateRoutes.jsx';
@@ -21,7 +21,7 @@ function App() {
         <Route path="/" element={<Auth />} /> 
         <Route path="/financetracker" element={<FinanceTracker />} />
         {/* {auth.isUser && auth.islogin && <Route path="/financetracker" element={<FinanceTracker />} />}  */}
-        {/* <Route path="*" element={<PageNotFound />} /> */}
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
   )
