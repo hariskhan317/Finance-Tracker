ce764c51bc6f99a3d6a69752c4e07940fba9ddad
Haris <harisgujjar317@gmail.com>
2024-05-27 23:30:56 -0400

    deployment

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 config();
 
 app.use(cors({
-    origin: ["https://finance-tracker-frontend-omega.vercel.app"],
+    origin: ["http://localhost:5173"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true
 }));
