6cf9aaa04e27f23d53f0d7898f0eea19de696d5e
Haris <harisgujjar317@gmail.com>
2024-05-28 14:46:22 -0400

    deploying on vercel-frontend -000

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,8 @@ import { AuthProvider } from './context/AuthContext.jsx';
 import { FinanceProvider } from './context/FinanceContext.jsx';
 
 
-axios.defaults.baseURL = "http://localhost:8000/api/v1";
+// axios.defaults.baseURL = "http://localhost:8000/api/v1";
+axios.defaults.baseURL = "https://finance-tracker-backend-azure.vercel.app/api/v1";
 axios.defaults.withCredentials = true
 
 ReactDOM.createRoot(document.getElementById('root')).render(
