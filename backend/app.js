fd5d1079657552cf325aba461742fbd08b8ac5d5
Haris <harisgujjar317@gmail.com>
2024-05-30 22:06:50 -0400

    cookie issue 003

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,19 @@ import express from 'express';
 import appRouter from './routes/index.js';
 import { config } from 'dotenv';
 import cookieParser from 'cookie-parser';
-import cors from 'cors'
+import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
 
 
 config();
+app.use(helmet())
+// app.use(cors({
+//     origin: ["http://localhost:5173"],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     credentials: true
+// }));
 app.use(cors({
     origin: ["https://finance-tracker-nine-murex.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
